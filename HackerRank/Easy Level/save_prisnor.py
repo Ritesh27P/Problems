ba764af717550d1043d007613d2173435620e12f
ritesh-blog27 <riteshminecraft27@gmail.com>
2022-11-12 19:03:18 +0530

    solved problem

---
+++ HackerRank/Easy Level/save_prisnor.py
@@ -0,0 +1,16 @@
+def save_prisnor(n, m, s):
+    candy = m
+    current_chair = s
+    while True:
+        if candy == 1:
+            return current_chair
+        candy -= 1
+        if current_chair == n:
+            current_chair = 1
+        else:
+            current_chair += 1
+        
+print(save_prisnor(5, 2, 1))
+print(save_prisnor(3, 7, 3))
+print(save_prisnor(7, 19, 2))
+    
