bc5a1bded1b25c006c5b9dccb63e0266fc0a33a0
ritesh-blog27 <riteshminecraft27@gmail.com>
2022-11-15 11:18:16 +0530

    solved problem

---
+++ LeetCode/Easy Level/tribonacci.py
@@ -0,0 +1,12 @@
+
+def tribonacci(n):
+    if n == 0:
+        return 0
+    elif n == 1:
+        return 1
+    elif n == 2:
+        return 1
+    else:
+        return tribonacci(n-1) + tribonacci(n-2) + tribonacci(n-3)
+
+print(tribonacci(29))
