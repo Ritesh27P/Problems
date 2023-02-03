45ac8ec8b389c6fa01c0d61b5008043fdd2dc1d3
ritesh-blog27 <riteshminecraft27@gmail.com>
2023-02-03 20:01:06 +0530

    solved valid palindrome problem

---
+++ LeetCode/Medium Level/multiply_string.py
@@ -0,0 +1,32 @@
+s = '2'
+
+match s:
+    case '1':
+        print(1)
+    case '2':
+        print(2)
+    case '3':
+        print(3)
+
+def toInt(s):
+    match s:
+        case '1':
+            return 1
+        case '2':
+            return 2
+        case '3':
+            return 3
+        case '4':
+            return 4
+        case '5':
+            return 5
+        case '6':
+            return 6
+        case '7':
+            return 7
+        case '8':
+            return 8
+        case '9':
+            return 9
+        case '0':
+            return 0
