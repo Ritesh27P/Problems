cdb897b3829e29bc9f71ed294c7456a804245ebe
ritesh-blog27 <riteshminecraft27@gmail.com>
2023-03-06 10:39:25 +0530

    solved problem

---
+++ LeetCode/Easy Level/k_th_missing_positive_Number.py
@@ -0,0 +1,11 @@
+class Solution:
+    def findKthPositive(self, arr: List[int], k: int) -> int:
+        pointer = 0
+        value = 0
+        while pointer < k:
+            value += 1
+            if value not in arr:
+                pointer += 1
+            
+        
+        return value
